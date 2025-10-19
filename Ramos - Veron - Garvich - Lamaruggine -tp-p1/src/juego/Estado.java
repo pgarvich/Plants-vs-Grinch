438e8ac1ad305e58e5eb5988568cec3c903b7fba
MintY <pigarvich@campus.ungs.edu.ar>
2025-10-19 11:38:38 -0300

    estados e imagen de pastos bis

--- Ramos - Veron - Garvich - Lamaruggine -tp-p1/src/juego/Estado.java
+++ Ramos - Veron - Garvich - Lamaruggine -tp-p1/src/juego/Estado.java
@@ -1,5 +1,47 @@
 package juego;
 
 public class Estado {
+	int estadoActual;
+	int estadoAnterior = 1;
+	
+	public static final int INICIO = 1;
+	public static final int JUGANDO = 2;
+	public static final int PAUSA = 3;
+	public static final int DERROTA = 4;
+	public static final int VICTORIA = 5;
 
+	public Estado() {
+
+		this.estadoActual = INICIO;
+	}
+	
+	public int getEstado() {	//getter
+		return estadoActual;
+	}
+	
+	public void setEstado(int nuevoEstado) {	//setter
+//		this.estadoAnterior = this.estadoActual;
+		this.estadoActual = nuevoEstado;
+	}
+	
+	//"getters" 
+	public boolean esInicio() {
+		return estadoActual == INICIO;
+	}
+	
+	public boolean esJuego() {
+		return estadoActual == JUGANDO;
+	}
+	
+	public boolean esPausa() {
+		return estadoActual == PAUSA;
+	}
+	
+	public boolean esDerrota(){
+		return estadoActual == DERROTA;
+	}
+	
+	public boolean esVictoria() {
+		return estadoActual == VICTORIA;
+	}
 }
