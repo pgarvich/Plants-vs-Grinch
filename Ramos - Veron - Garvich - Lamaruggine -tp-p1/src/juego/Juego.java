69bda00d2df490ec708dcf835568f84c33c6fd03
MintY <pigarvich@campus.ungs.edu.ar>
2025-11-13 15:13:34 -0300

    Migrada la lógica de los regalos de jardín a main

--- Ramos - Veron - Garvich - Lamaruggine -tp-p1/src/juego/Juego.java
+++ Ramos - Veron - Garvich - Lamaruggine -tp-p1/src/juego/Juego.java
@@ -1,6 +1,7 @@
 package juego;
 
 import entorno.Entorno;
+import entorno.Herramientas;
 import entorno.InterfaceJuego;
 
 public class Juego extends InterfaceJuego
@@ -27,6 +28,118 @@ public class Juego extends InterfaceJuego
  	    this.combate = new Combate(this.entorno, this.jardin, this.cripta, this.estado, this.reloj);
 	    
 	    this.entorno.iniciar();
+	}
+	
+	public void dibujarRegalos() {
+		if(estado.esDerrota()) {
+			if(cripta.regalo1)
+				entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 226, 0);
+			if(cripta.regalo2)
+				entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 353, 0);
+			if(cripta.regalo3)
+				entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 482, 0);
+			if(cripta.regalo4)
+				entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 612, 0);
+			if(cripta.regalo5)
+				entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 736, 0);
+		}
+		
+		if(estado.esJuego() || estado.esVictoria()) {
+		if (reloj.ciclos(200, 1200)) {			//1er regalo
+    		entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 226, 0);
+		}
+		else if (reloj.ciclos(400,  1200)) {
+    		entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 220, 0);
+		}
+		else if (reloj.ciclos(600,  1200)) {
+    		entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 216, 0);
+		}
+		else if (reloj.ciclos(800,  1200)) {
+    		entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 210, 0);
+		}
+		else if (reloj.ciclos(1000,  1200)) {
+    		entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 204, 0);
+		}
+		else {
+    		entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 198, 0);
+		}
+		
+		if (reloj.ciclos(200, 1200)) {		//segundo
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 353, 0);
+		}
+		else if (reloj.ciclos(400, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 347, 0);
+		}
+		else if (reloj.ciclos(600, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 341, 0);
+		}
+		else if (reloj.ciclos(800, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 335, 0);
+		}
+		else if (reloj.ciclos(1000, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 329, 0);
+		}
+		else {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 323, 0);
+		}
+		
+		if (reloj.ciclos(200, 1200)) { //tercero
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 482, 0);
+		}
+		else if (reloj.ciclos(400, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 476, 0);
+		}
+		else if (reloj.ciclos(600, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 470, 0);
+		}
+		else if (reloj.ciclos(800, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 464, 0);
+		}
+		else if (reloj.ciclos(1000, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 458, 0);
+		}
+		else {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 452, 0);
+		}
+		
+		if (reloj.ciclos(200, 1200)) {		//cuarto
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 612, 0);
+		}
+		else if (reloj.ciclos(400, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 606, 0);
+		}
+		else if (reloj.ciclos(600, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 600, 0);
+		}
+		else if (reloj.ciclos(800, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 594, 0);
+		}
+		else if (reloj.ciclos(1000, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 588, 0);
+		}
+		else {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 582, 0);
+		}
+		
+		if (reloj.ciclos(200, 1200)) {		//quinto
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 736, 0);
+		}
+		else if (reloj.ciclos(400, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 730, 0);
+		}
+		else if (reloj.ciclos(600, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 724, 0);
+		}
+		else if (reloj.ciclos(800, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 718, 0);
+		}
+		else if (reloj.ciclos(1000, 1200)) {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 712, 0);
+		}
+		else {
+		    entorno.dibujarImagen(Herramientas.cargarImagen("personajes/regalo1.png"), 55, 706, 0);
+		}
+		}
 	}
 
 	/**
@@ -70,7 +183,7 @@ public class Juego extends InterfaceJuego
 		}
 		cripta.verZColosal();
 		combate.debilitamiento();
-		jardin.dibujarRegalos();
+		dibujarRegalos();
 		jardin.victoriaPlanta();
 		if(estado.esDerrota()) {
 			cripta.zombiesVictoriosos();
